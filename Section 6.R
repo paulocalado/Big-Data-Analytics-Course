e42d196f218ede68bb98511f8ec029eaf5448607
Paulo Henrique Calado Aoun <paulo_calado@live.com>
2018-09-18 22:43:15 -0300

    PCA and K-Means

--- Section 6.R
+++ Section 6.R
@@ -43,3 +43,54 @@ flights_tbl%>%
   ft_bucketizer("distance","distance_cat",
                 splits = c(0,500,1500,Inf))%>%
   select(distance, distance_cat)
+
+###PCA
+iris_tbl<- copy_to(sc, iris, "iris",overwrite = TRUE)
+
+pca_model<- tbl(sc,"iris") %>%
+  select(-Species)%>%
+  ml_pca()
+
+print(pca_model)
+
+D <- as.matrix(iris[1:4])
+E <- as.matrix(pca_model$pc)
+P <-  D %*% E
+
+PCs <- as.data.frame(P)
+PCs$Species <- iris$Species
+
+ggplot(PCs,aes(PC1,PC2)) + geom_point(aes(colour = iris$Species))
+
+###K-MEANS
+kmeans_model <- iris_tbl %>% 
+  ml_kmeans(formula= ~ Petal_Width + Petal_Length, centers = 3)
+
+print(kmeans_model)
+
+sdf_predict(kmeans_model) %>%
+  collect() %>%
+  ggplot(aes(Petal_Length, Petal_Width)) +
+  geom_point(aes(Petal_Width, Petal_Length, col = factor(prediction+1)),
+             size = 2, alpha = 0.5) + 
+  geom_point(data = kmeans_model$centers, aes(Petal_Width, Petal_Length),
+             pch = 'x', size = 12) +
+  scale_color_discrete(name = "Predicted cluster")
+
+##ACTIVITY
+install.packages("rattle.data")
+wine <- rattle.data::wine
+
+wine[-1] <- scale(wine[-1])
+
+wine_tbl<- copy_to(sc, wine, "wine",overwrite = TRUE)
+wine_pca_model<- tbl(sc,"wine")%>%
+  select(-Type)%>%
+  ml_pca()
+
+print(wine_pca_model)
+
+kmeans_model_wine<- wine_tbl%>%
+  ml_kmeans(formula= ~Nonflavanoids + Hue, centers = 3)
+
+print(kmeans_model_wine)
